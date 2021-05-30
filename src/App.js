07826d4607671f087545e9b43438fc036e781374
mgrok <mgrok@mgroks-MacBook-Air.local>
2021-05-30 11:12:59 -0400

    setting up *our* project

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ const App = () => {
         setScore(localStorage.getItem('high'))
         // TODO: 
         // reasign score to scanned wood
+        // invite collaborators for feedback
         console.log(score)
       }, 1106*10)
       return () => clearTimeout(timer);
